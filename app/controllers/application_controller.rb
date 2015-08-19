175449d28f9648fcd2c5afc30489ca88853aa867
Marcelo Soares Souza <marcelo@libertais.org>
2015-08-19 08:38:42 -0300

    Correção no Cadastro de CPF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nome, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nome, :email, :password, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nome, :cpf, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nome, :cpf, :email, :password, :current_password) }
   end  
 end
